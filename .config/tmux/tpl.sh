42456b9a240bfeaf552dec259de03f49c656d4a9
ddnomad <ddnomad@pm.me>
2020-08-11 02:49:31 +0300

    Tiny fixes for Tmux Powerline integration

--- .config/tmux/tpl.sh
+++ .config/tmux/tpl.sh
@@ -8,17 +8,11 @@ readonly TPL_OLD_CFG='$HOME/.tmux-powerlinerc'
 # shellcheck disable=SC2016
 readonly TPL_NEW_CFG='$HOME/.config/tmux/tpl.conf'
 readonly TPL_PATHS_FILE="${TPL_DIR}/config/paths.sh"
+readonly TPL_NOW_PLAYING_FILE="${TPL_DIR}/segments/now_playing.sh"
 readonly TPL_SPOTIFY_MAC_FILE="${TPL_DIR}/segments/np_spotify_mac.script"
+readonly TPL_WAN_IP_FILE="${TPL_DIR}/segments/wan_ip.sh"
 
-function init {
-    if test -f "${TPL_DIR}"; then
-        echo 'Error: Already initialized'
-        exit 1
-    fi
-
-    echo 'Initializing Tmux Powerline ...'
-    git clone "${TPL_ORIGIN}" "${TPL_DIR}"
-
+function patch {
     echo 'Patching Tmux Powerline config path ...'
     gsed \
         -i \
@@ -28,12 +22,35 @@ function init {
     echo 'Patching Tmux Powerline Now Playing segment ...'
     gsed \
         -i \
-        's@\(\t*.*to "\)►\(.*\)@\1\2@' \
+        's@♫@@' \
+        "${TPL_NOW_PLAYING_FILE}"
+    gsed \
+        -i \
+        's@►@ @' \
         "${TPL_SPOTIFY_MAC_FILE}"
     gsed \
         -i \
-        's@\(\t*.*to "\)❙❙\(.*\)@\1\2@' \
+        's@❙❙@ @' \
         "${TPL_SPOTIFY_MAC_FILE}"
+
+    echo 'Patching Tmux Powerline WAN IP segment ...'
+    gsed \
+        -i \
+        's@ⓦ @  @' \
+        "${TPL_WAN_IP_FILE}"
+}
+
+function init {
+    if test -f "${TPL_DIR}"; then
+        echo 'Error: Already initialized'
+        exit 1
+    fi
+
+    echo '---(i) Initializing Tmux Powerline ...'
+    git clone "${TPL_ORIGIN}" "${TPL_DIR}"
+
+    echo '---(i) Patching Tmux Powerline ...'
+    patch
 }
 
 function main {
