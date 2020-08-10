7339b6bee03bb117cd179b5632da111c99025686
ddnomad <ddnomad@pm.me>
2020-08-11 02:03:09 +0300

    finished integration of Tmux Powerline

--- .config/tmux/tpl.sh
+++ .config/tmux/tpl.sh
@@ -8,6 +8,7 @@ readonly TPL_OLD_CFG='$HOME/.tmux-powerlinerc'
 # shellcheck disable=SC2016
 readonly TPL_NEW_CFG='$HOME/.config/tmux/tpl.conf'
 readonly TPL_PATHS_FILE="${TPL_DIR}/config/paths.sh"
+readonly TPL_SPOTIFY_MAC_FILE="${TPL_DIR}/segments/np_spotify_mac.script"
 
 function init {
     if test -f "${TPL_DIR}"; then
@@ -23,6 +24,16 @@ function init {
         -i \
         's@^\(.*\)'"${TPL_OLD_CFG}"'\(.*\)$@\1'"${TPL_NEW_CFG}"'\2@' \
         "${TPL_PATHS_FILE}"
+
+    echo 'Patching Tmux Powerline Now Playing segment ...'
+    gsed \
+        -i \
+        's@\(\t*.*to "\)►\(.*\)@\1\2@' \
+        "${TPL_SPOTIFY_MAC_FILE}"
+    gsed \
+        -i \
+        's@\(\t*.*to "\)❙❙\(.*\)@\1\2@' \
+        "${TPL_SPOTIFY_MAC_FILE}"
 }
 
 function main {
